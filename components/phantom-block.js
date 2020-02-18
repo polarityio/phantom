592685d8b999b14d67ba33981a5609418a8323f0
Ed <ed@polarity.io>
2020-02-18 13:32:04 -0500

    Simplify component logic for running playbooks and update layout

--- components/phantom-block.js
+++ components/phantom-block.js
@@ -1,25 +1,21 @@
-let playbookId;
-
 polarity.export = PolarityComponent.extend({
-    details: Ember.computed.alias('block.data.details'),
-    actions: {
-        runPlaybook: function (containerId, entity) {
-            let self = this;
-            self.set('message', null);
+  details: Ember.computed.alias('block.data.details'),
+  actions: {
+    runPlaybook: function(containerId, playbookId, entity) {
+      let self = this;
+      self.set('message', null);
 
-            console.log('sending message with cont id ' + containerId + ' play id' + playbookId);
-            
-            this.sendIntegrationMessage({ data: { containerId, playbookId, entity } })
-                .then(function (/* response */) {
-                    self.set('message', "Success!");
-                }).catch(function (err) {
-                    console.error(err);
-                    self.set('message', "Error running playbook");
-                });
-        },
-        onSelectPlaybook: function (value) {
-            playbookId = value;
-        }
-    },
-    message: ''
+      console.info('sending message with cont id ' + containerId + ' play id' + playbookId);
+
+      this.sendIntegrationMessage({ data: { containerId, playbookId, entity } })
+        .then(function(/* response */) {
+          self.set('message', 'Success!');
+        })
+        .catch(function(err) {
+          console.error(err);
+          self.set('message', 'Error running playbook');
+        });
+    }
+  },
+  message: ''
 });
