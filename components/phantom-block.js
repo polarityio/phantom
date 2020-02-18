0e9f1bd89304c171dd822548301c1ac3075f11c8
Ed <ed@polarity.io>
2020-02-18 15:12:59 -0500

    Use per-container messages and additional layout cleanup

--- components/phantom-block.js
+++ components/phantom-block.js
@@ -1,21 +1,30 @@
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
+  containers: Ember.computed.alias('details.results'),
   actions: {
-    runPlaybook: function(containerId, playbookId, entity) {
+    runPlaybook: function(containerIndex, containerId, playbookId, entity) {
       let self = this;
       self.set('message', null);
 
-      console.info('sending message with cont id ' + containerId + ' play id' + playbookId);
+      if(!playbookId){
+        return this.setMessage(containerIndex, 'Select a playbook to run.');
+      }
+
+      console.info(`sending message with cont id ${containerId} and playbook id ${playbookId}`);
 
       this.sendIntegrationMessage({ data: { containerId, playbookId, entity } })
         .then(function(/* response */) {
           self.set('message', 'Success!');
+          self.setMessage(containerIndex, 'Success!');
         })
         .catch(function(err) {
           console.error(err);
-          self.set('message', 'Error running playbook');
+          self.setMessage(containerIndex, 'Error running playbook');
         });
     }
+  },
+  setMessage(containerIndex, msg){
+    this.set(`containers.${containerIndex}.__message`, msg);
   },
   message: ''
 });
